89a23092c2b3aff4edf2dd9b989355fe5dba1a2a
LucasKnezevich <knezevich.lucas@gmail.com>
2023-04-26 13:39:00 -0700

    Updated fetch to localhost.

--- front-end/app/components/SearchView.jsx
+++ front-end/app/components/SearchView.jsx
@@ -1,15 +1,27 @@
 'use client';
 import { Table } from "antd";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const SearchView = () => {
   const [query, setQuery] = useState('');
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     console.log("Query: " + query)
+    fetch(`http://localhost:3000/${query}`)
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(err => console.error('Error: ' + err.message));
+    console.log(data);
+    if (data.length > 0) {
+      loadCols(data);
+      setColumns(cols);
+    }
+    setLoading(false);
   };
 
   // Pull column/field names
@@ -26,18 +38,6 @@ const SearchView = () => {
     }
   }
 
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${query}`)
-    .then(res => res.json())
-    .then(data => setData(data))
-    .catch(err => console.log(err.message));
-
-    if (data.length > 0) {
-      loadCols(data);
-      setColumns(cols);
-    }
-  }, [data]);
-
   return (
     <>
       <form className="search-form" action="" onSubmit={handleSubmit}>
@@ -51,9 +51,9 @@ const SearchView = () => {
       </form>
 
 
-      {data.length === 0 ? 
+      {loading ? 
         <span className="loader"></span> : 
-        <Table 
+        <Table
         className="results-table"
         dataSource={data}
         columns={columns}

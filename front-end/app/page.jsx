d262ccaf0b39682788181a685d5cb9addbec157b
ariunzayaE <ariunzaya.eb@gmail.com>
2023-04-26 19:52:36 -0700

    merged main branch

--- front-end/app/page.jsx
+++ front-end/app/page.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import ResultsTable from "./components/ResultsTable"
-import SearchBar from "./components/SearchBar"
-=======
 import SearchView from "./components/SearchView"
->>>>>>> 340d43d61175c83bddf4bea5211ba6fa6dc08092
 
 const HomePage = () => {
   return (
     <>
       <div className="container">
         <h1 className="page-title">Database Search</h1>
-<<<<<<< HEAD
-        <SearchBar />
-        <div className="result-table-container">
-          <ResultsTable />
-        </div>
-=======
         <SearchView />
->>>>>>> 340d43d61175c83bddf4bea5211ba6fa6dc08092
       </div>
     </>
   )

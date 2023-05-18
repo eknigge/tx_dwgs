a8ed6e54f67d9a7b1f18b39220c77c4b13b2051e
Zaya Erdenebileg <81658998+ariunzayaE@users.noreply.github.com>
2023-05-17 20:21:34 -0700

    changed font, background, underline etc (#70)

--- front-end/app/components/Header.jsx
+++ front-end/app/components/Header.jsx
@@ -5,13 +5,16 @@ import Dropdown from "./Dropdown"
 const Header = () => {
   return (
     <header className="header">
+  <div className="image-container">
       <Image
         src={logo}
         width={200}
         height={110}
         alt="Snohomish PUD Logo"
         priority
+        className="header-image"
       />
+      </div>
       <Dropdown />
       <h1>Transmission Drawings</h1>
     </header>

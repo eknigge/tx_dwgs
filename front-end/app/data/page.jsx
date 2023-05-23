1bcaa6384060688c511ded35d2ea7950e7c5df76
Zaya Erdenebileg <81658998+ariunzayaE@users.noreply.github.com>
2023-05-22 21:09:00 -0700

    embedded Tableau Public in Data page (#90)
    * embedded Tableau Public in Data page
    
    * Update import to see if this passes linting.
    
    ---------
    
    Co-authored-by: Lucas Knezevich <46425585+LucasKnezevich@users.noreply.github.com>

--- front-end/app/data/page.jsx
+++ front-end/app/data/page.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+'use client';
+import { React, useEffect } from 'react';
+
+const TableauVisualization = () => {
+useEffect(() => {
+  const scriptElement = document.createElement('script');
+  scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+  document.getElementById('viz1684472411252').appendChild(scriptElement);
+
+  return () => {
+
+    document.getElementById('viz1684472411252').removeChild(scriptElement);
+  };
+}, []);
+
 const Data = () => {
   return (
-    <>
-      <div className="container">
-        <h1 className="page-title">Data</h1>
+    <div className="tableauContainer">
+      <div className="tableauPlaceholder" id="viz1684472411252" style={{ position: 'relative' }}>
+        <noscript>
+          <a href="#">
+            <img
+              alt="Manufacturer and Install Date"
+              src="https://public.tableau.com/static/images/Ma/ManufacturerandInstalldate/Story2/1_rss.png"
+              style={{ border: 'none' }}
+            />
+          </a>
+        </noscript>
+        <object className="tableauViz" style={{ display: 'none' }}>
+          <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
+          <param name="embed_code_version" value="3" />
+          <param name="site_root" value="" />
+          <param name="name" value="ManufacturerandInstalldate/Story2" />
+          <param name="tabs" value="no" />
+          <param name="toolbar" value="yes" />
+          <param name="static_image" value="https://public.tableau.com/static/images/Ma/ManufacturerandInstalldate/Story2/1.png" />
+          <param name="animate_transition" value="yes" />
+          <param name="display_static_image" value="yes" />
+          <param name="display_spinner" value="yes" />
+          <param name="display_overlay" value="yes" />
+          <param name="display_count" value="yes" />
+          <param name="language" value="en-US" />
+          <param name="filter" value="publish=yes" />
+        </object>
       </div>
-    </>
-  )
-}
+      
 
-export default Data
+      
+    </div>
+  );
+};
+
+export default TableauVisualization;

adb66f771ccf12f0ae601dbe370be9c1d835e0fc
Lucas Knezevich <46425585+LucasKnezevich@users.noreply.github.com>
2023-06-01 18:42:07 -0700

    LFK 81 - Modularize front-end API Requests (#104)

--- front-end/app/components/AdminView.jsx
+++ front-end/app/components/AdminView.jsx
@@ -1,6 +1,8 @@
 'use client'
 import { React, useState } from 'react'
-import { Button, Table, Popconfirm, message } from 'antd'
+import { Button, Table, Popconfirm } from 'antd'
+import { FetchQueryData } from '../api/FetchQueryData'
+import { DeleteRecord } from '../api/DeleteRecord'
 
 const AdminView = () => {
   const [query, setQuery] = useState('')
@@ -11,26 +13,15 @@ const AdminView = () => {
   const [error, setError] = useState(false)
   const [apiKey, setApiKey] = useState('')
 
-  const fetchData = async () => {
+  const loadData = async () => {
     setNoResultsFound(false)
     setError(false)
     setLoading(true)
-    try {
-      const response = await fetch('http://localhost:3000', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          query
-        })
-      })
-      const jsonData = await response.json()
-      setData(jsonData)
-      setLoading(false)
 
-      if (jsonData.length > 0) {
-        const columnKeys = Object.keys(jsonData[0])
+    try {
+      const res = await FetchQueryData(query)
+      if (res.length > 0) {
+        const columnKeys = Object.keys(res[0])
         const dynamicColumns = [
           ...columnKeys.map((key) => ({
             title: key,
@@ -56,9 +47,13 @@ const AdminView = () => {
           }
         ]
         setColumns(dynamicColumns)
+        setData(res)
       } else {
         setNoResultsFound(true)
+        setColumns([])
+        setData([])
       }
+      setLoading(false)
     } catch (error) {
       console.error('Error fetching data:', error)
       setData([])
@@ -67,38 +62,9 @@ const AdminView = () => {
     }
   }
 
-  const deleteData = async (record) => {
-    console.log('Delete record: ' + record)
-    try {
-      const response = await fetch('http://localhost:3000/admin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          query: record,
-          api_key: apiKey,
-          operation: 'delete'
-        })
-      })
-      const res = await response.text()
-      console.log('Response: ' + res)
-      if (res === 'success') {
-        message.success('Record deleted successfully.')
-      } else if (res === 'Bad API Key') {
-        message.error('Bad API Key')
-      } else {
-        message.warning(res)
-      }
-    } catch (error) {
-      console.error('Error deleting data:', error)
-      message.error('Error deleting record.')
-    }
-  }
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    fetchData()
+    await loadData()
   }
 
   const handleDelete = async (record) => {
@@ -107,8 +73,8 @@ const AdminView = () => {
       rec = record[key]
       break
     }
-    await deleteData(rec)
-    fetchData()
+    await DeleteRecord(apiKey, rec)
+    await loadData()
   }
 
   return (

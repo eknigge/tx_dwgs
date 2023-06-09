5aa2ccbe6341f7c1330e8f9c700219a25b20f71e
ariunzayaE <ariunzaya.eb@gmail.com>
2023-06-08 19:45:43 -0700

    updated Tableau story

--- front-end/app/data/page.jsx
+++ front-end/app/data/page.jsx
@@ -23,15 +23,8 @@ const TableauVisualization = () => {
               style={{ border: 'none' }}
             />
           </a>
-          <a href="#">
-            <img
-              alt="Pole Detailed Information"
-              src="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sn&#47;SnohomishCountyPUD-TransmissionPoleDetails&#47;
-              Story1&#47;1_rss.png"
-              style={{ border: 'none' }}
-            />
-          </a>
         </noscript>
+
         <object className="tableauViz" style={{ display: 'none' }}>
           <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
           <param name="embed_code_version" value="3" />
@@ -60,6 +53,7 @@ const TableauVisualization = () => {
             />
           </a>
         </noscript>
+
         <object className="tableauViz" style={{ display: 'none' }}>
           <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
           <param name="embed_code_version" value="3" />
@@ -77,8 +71,35 @@ const TableauVisualization = () => {
           <param name="language" value="en-US" />
           <param name="filter" value="publish=yes" />
         </object>
-      </div>
 
+      
+        <noscript>      
+        <a href="#">
+          <img 
+          alt="Drawing, Line, and Pole information"
+          src="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;fi&#47;final_16862689625800&#47;Story1&#47;1_rss.png"
+          style={{ border: 'none'}}
+           />
+          </a>
+        </noscript>
+        <object className="tableauViz" style={{ display: 'none' }}>
+        <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
+        <param name="embed_code_version" value="3" />
+        <param name="site_root" value="" />
+        <param name="name" value="final_16862689625800&#47;Story1" />
+        <param name="tabs" value="no" />
+        <param name="toolbar" value="yes" />
+        <param name="static_image" value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;fi&#47;final_16862689625800&#47;Story1&#47;1.png" />
+        <param name="animate_transition" value="yes" />
+        <param name="display_static_image" value="yes" />
+        <param name="display_spinner" value="yes" />
+        <param name="display_overlay" value="yes" />
+        <param name="display_count" value="yes" />
+        <param name="language" value="en-US" />
+        <param name="filter" value="publish=yes" />
+      </object>
+    </div>
+       
   )
 }
 
